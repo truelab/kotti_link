83899f533c91acb6f421c8ef1c33824df50cbeeb
Marco Scheidhuber <j23d@jusid.de>
2014-01-24 15:30:04 +0100

    Back to dev.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 project = 'kotti_link'
-version = '0.1a1'
+version = '0.1a2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
