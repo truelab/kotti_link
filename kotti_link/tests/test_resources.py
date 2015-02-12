30782deebd16161b4c53f9522089495e10841f3f
davidemoro <davide.moro@gmail.com>
2015-02-12 18:05:17 +0100

    tests cleaning. Imports should be placed inside tests, best practice

--- kotti_link/tests/test_resources.py
+++ kotti_link/tests/test_resources.py
@@ -1,7 +1,7 @@
-from kotti_link.resources import Link
 
 
 def test_link(db_session, root):
+    from kotti_link.resources import Link
 
     content = Link(link='')
     # assert content.type_info.addable(content, DummyRequest()) is True
@@ -10,12 +10,14 @@ def test_link(db_session, root):
 
 def test_link_attributes(db_session, root):
     """ The link attribute should be set """
+    from kotti_link.resources import Link
     root['content'] = Link(link=u'http://google.com')
     assert root['content'].link == u'http://google.com'
 
 
 def test_link_copy(db_session, root):
     """ Link objects should be copied """
+    from kotti_link.resources import Link
     root['content'] = Link(link='http://google.com')
     root['copy-of'] = root['content'].copy(title='another link')
     assert root['copy-of'].title == u'another link'
