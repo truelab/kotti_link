a1e502aeb0953b749f0245c0e444d216cdf6ce59
davidemoro <davide.moro@gmail.com>
2015-02-12 18:03:35 +0100

    removes unused imports

--- kotti_link/tests/test_resources.py
+++ kotti_link/tests/test_resources.py
@@ -1,12 +1,8 @@
-from kotti.resources import get_root
-from kotti.testing import DummyRequest
-
 from kotti_link.resources import Link
 
 
-def test_link(db_session, config):
+def test_link(db_session, root):
 
-    root = get_root()
     content = Link(link='')
     # assert content.type_info.addable(content, DummyRequest()) is True
     root['content'] = content
